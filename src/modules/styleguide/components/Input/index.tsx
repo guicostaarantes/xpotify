b2d4f686c2697e4abf807158422a6caf76a78ec1
Guilherme Arantes <arantes.g@outlook.com>
2021-10-14 19:06:42 -0300

    💄 added input component

---
+++ src/modules/styleguide/components/Input/index.tsx
@@ -0,0 +1,26 @@
+import React, { BaseHTMLAttributes, RefObject } from "react";
+
+import styles from "./style.css";
+
+type InputProps = BaseHTMLAttributes<HTMLInputElement> & {
+  innerRef?: RefObject<HTMLInputElement>;
+  value?: string;
+};
+
+const Input = ({
+  className = "",
+  innerRef,
+  onChange,
+  value,
+  ...rest
+}: InputProps) => (
+  <input
+    className={`${styles.input} ${className}`}
+    onChange={onChange}
+    ref={innerRef}
+    value={value}
+    {...rest}
+  />
+);
+
+export default Input;
