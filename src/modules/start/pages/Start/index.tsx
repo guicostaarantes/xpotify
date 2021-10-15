896d0f41a8e326dba0f11dcc41927f7df0fecb87
Guilherme Arantes <arantes.g@outlook.com>
2021-10-15 17:50:57 -0300

    ✨ added albums to search result

--- src/modules/start/pages/Start/index.tsx
+++ src/modules/start/pages/Start/index.tsx
@@ -1,24 +1,37 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { ApplicationState } from "#/shared/store";
-import { setSearchStringDebounced } from "#/shared/store/search";
+import {
+  setSearchResultFromSearchString,
+  setSearchStringDebounced,
+} from "#/shared/store/search";
+import AlbumCard from "#/styleguide/components/AlbumCard";
 import Input from "#/styleguide/components/Input";
 
 import styles from "./style.css";
 
 const StartPage = () => {
+  const searchResult = useSelector(
+    (store: ApplicationState) => store.search.searchResult,
+  );
   const searchString = useSelector(
     (store: ApplicationState) => store.search.searchString,
   );
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (searchString) dispatch(setSearchResultFromSearchString(searchString));
+  }, []);
 
   const handleSetSearchString = (event: ChangeEvent<HTMLInputElement>) =>
     dispatch(setSearchStringDebounced(event.target.value, 500));
 
   return (
     <div className={styles.container}>
-      <label htmlFor="search">Busque por artistas, álbuns ou músicas</label>
+      <label className={styles.searchLabel} htmlFor="search">
+        Busque por artistas, álbuns ou músicas
+      </label>
       <Input
         className={styles.bigInput}
         id="search"
@@ -26,6 +39,21 @@ const StartPage = () => {
         placeholder="Comece a escrever..."
         value={searchString}
       />
+      {searchResult.albums?.items ? (
+        <>
+          <h3 className={styles.sectionTitle}>Álbuns</h3>
+          <div className={styles.albumCardGrid}>
+            {searchResult.albums.items.map((album) => (
+              <AlbumCard
+                key={album.id}
+                src={album.images[1].url}
+                primaryText={album.name}
+                secondaryText={album.artists[0].name}
+              />
+            ))}
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
---
+++ src/modules/styleguide/components/AlbumCard/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import styles from "./style.css";
+
+type AlbumCardProps = {
+  src: string;
+  primaryText: string;
+  secondaryText: string;
+};
+
+const AlbumCard = ({ src, primaryText, secondaryText }: AlbumCardProps) => (
+  <div className={styles.container}>
+    <img className={styles.img} src={src} alt={primaryText} />
+    <div className={styles.primaryText}>{primaryText}</div>
+    <div className={styles.secondaryText}>{secondaryText}</div>
+  </div>
+);
+
+export default AlbumCard;
